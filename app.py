c8f8eaa923fcb4948f6638bcbb0ee5404d240656
HariJohan <nivasanhari@gmail.com>
2026-04-28 15:14:30 +0530

    flask application

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello!</h1><p>Running locally on Python 3.14</p>'
+
+if __name__ == '__main__':
+    # Running on port 5000
+    app.run(debug=True, port=5000)
